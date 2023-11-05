cfd2f6e6581d973fab196208f4d64cedde436c6b
Romain Jolidon <romain.jolidon@epitech.eu>
2023-11-05 14:20:43 +0100

    fix asset url

--- assets/index.be41b02e.js
+++ assets/index.be41b02e.js
@@ -4352,7 +4352,7 @@ function instance$o($$self, $$props, $$invalidate) {
     }
     if ($$self.$$.dirty[1] & 16) {
       $$invalidate(13, foilStyles = `
-    --foil: url(${window.location.href}assets/shiny_masks/${foilId}');
+    --foil: url(https://romainjolidon.github.io/cardgame-shiny-presets/assets/shiny_masks/${foilId}');
     `);
     }
     if ($$self.$$.dirty[1] & 1032192) {
@@ -12215,4 +12215,4 @@ class App extends SvelteComponent {
 new App({
   target: document.getElementById("app")
 });
-//# sourceMappingURL=index.eef61a4b.js.map
+//# sourceMappingURL=index.be41b02e.js.map
